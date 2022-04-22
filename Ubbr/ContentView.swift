d33a22ac403e6f2f639dc84bff0d240edf87c65a
chenya <2227237581@qq.com>
2022-04-22 19:35:52 +0800

    Initial Commit

---
+++ Ubbr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ubbr
+//
+//  Created by 陈亚 on 2022/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ubbr/UbbrApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbbrApp.swift
+//  Ubbr
+//
+//  Created by 陈亚 on 2022/4/22.
+//
+
+import SwiftUI
+
+@main
+struct UbbrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
